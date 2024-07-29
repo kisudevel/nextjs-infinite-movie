057f55cdc648abb3bd0cb509ac0057b375b2d749
suya <suya@suyaui-MacBookPro.local>
2024-07-30 01:42:11 +0900

    intialized

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
       </article>
       {/* 무비 에어리어 */}
       <article className="bg-black py-10 px-4 xs:px-0">
-        <section className="container mx-auto py-8 text-white">
+        <section className="container mx-auto text-white">
           <span className="text-yellow-600">ONLINE STREAMING</span>
           <h2 className="text-[36px] font-bold mb-8">Movie LIST</h2>
           <div className="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 sm:px-0">
